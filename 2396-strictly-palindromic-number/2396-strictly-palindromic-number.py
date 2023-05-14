d6534ead24c4fb063a8102a398d6ee5729cfae3a
va1afar (Park JinGyu) <62978894+va1afar@users.noreply.github.com>
2023-05-14 20:42:26 +0900

    Time: 40 ms (27.21%), Space: 16.3 MB (14.68%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.py
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.py
@@ -4,9 +4,9 @@ class Solution:
         # making 2 bin number strings to compare.
         # even, odd lengths can be handled regardless of both.
         bin_n_left = str(bin_n[:len(bin_n)%2])
-        bin_n_right = str(bin_n[len(bin_n)%2:])
+        bin_n_right = str(bin_n[len(bin_n)%2::-1])
                               
-        if bin_n_left == bin_n_right[::-1]:
+        if bin_n_left == bin_n_right:
             return True
         else:
             return False
