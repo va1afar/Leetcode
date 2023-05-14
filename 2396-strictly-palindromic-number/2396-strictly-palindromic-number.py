c26cdc3152501ca31ef64905a471c6cb452eeccc
va1afar (Park JinGyu) <62978894+va1afar@users.noreply.github.com>
2023-05-14 20:15:28 +0900

    Time: 52 ms (10.91%), Space: 16.3 MB (14.68%) - LeetHub

---
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isStrictlyPalindromic(self, n: int) -> bool:
+        bin_n = format(n, 'b')
+        # making 2 bin number strings to compare.
+        # even, odd lengths can be handled regardless of both.
+        bin_n_left = str(bin_n[:len(bin_n)%2])
+        bin_n_right = str(bin_n[len(bin_n)%2:])
+                              
+        if bin_n_left == bin_n_right[::-1]:
+            return True
+        else:
+            return False
+                
