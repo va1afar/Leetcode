31a9533cec36a3040fecf252ea5b30016585fb70
va1afar (Park JinGyu) <62978894+va1afar@users.noreply.github.com>
2023-05-22 21:18:10 +0900

    Time: 2562 ms (16.90%), Space: 16.6 MB (34.27%) - LeetHub

---
+++ 1828-queries-on-number-of-points-inside-a-circle/1828-queries-on-number-of-points-inside-a-circle.py
@@ -0,0 +1,17 @@
+import math
+
+class Solution:
+    def countPoints(self, points: List[List[int]], queries: List[List[int]]) -> List[int]:
+        answer = []
+        
+        # 直角三角形の性質である、a^2 + b^2 = c^2 (cは対角線)を逆に利用し、
+        # a^2 + b^2 が query[2]**2以下かどうか確認します。
+        for query in queries:
+            dots_in_query = 0
+            for point in points:
+                if (query[0] - point[0])**2 + (query[1] - point[1])**2 <= query[2]**2:
+                    dots_in_query += 1
+            answer.append(dots_in_query)
+            
+        return answer
+                    
