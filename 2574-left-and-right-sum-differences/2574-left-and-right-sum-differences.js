6c6068e030053a04e41bba83abfd1e2512e60638
va1afar (Park JinGyu) <62978894+va1afar@users.noreply.github.com>
2023-06-18 21:44:30 +0900

    Time: 78 ms (52.60%), Space: 45.5 MB (43.30%) - LeetHub

---
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var leftRightDifference = function(nums) {
+    let leftSum = [];
+    let rightSum = [];
+    let outputArr = [];
+    leftSum = [0];
+    rightSum[nums.length-1] = 0;
+    
+    if(nums.length == 1){
+        return [0];
+    }
+
+    for(let i=1; i<nums.length; i++){
+        leftSum[i] = leftSum[i-1] + nums[i-1];
+    }
+    
+    for(let i=nums.length-2; i>=0; i--){
+        rightSum[i] = rightSum[i+1] + nums[i+1]
+    }
+
+    for(let i=0; i<nums.length; i++){
+        outputArr.push(Math.abs(leftSum[i]-rightSum[i]))
+    }
+
+    return outputArr;
+
+};
